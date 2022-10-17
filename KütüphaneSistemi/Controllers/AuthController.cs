e343ffb625825bedb4690537c3e5387aed48b6c5
haticebingol <haticebingol@HATICEBINGO4C4C>
2022-10-17 19:49:11 +0300

    Add project files.

---
+++ KütüphaneSistemi/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KütüphaneSistemi.Controllers
+{
+    public class AuthController : Controller
+    {     
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
